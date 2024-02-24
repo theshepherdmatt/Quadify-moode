8db8a1f04969fcf896372edfb2ca30d468951f12
Quadify <matt.theshepherd@gmail.com>
2024-02-24 18:42:06 +0000

    Update index.js

--- oled/index.js
+++ oled/index.js
@@ -478,9 +478,9 @@ ap_oled.prototype.playback_mode = function(){
 				// Clear the area before drawing new text
 				// Assuming there's a method to draw a rectangle, fillRect(x, y, width, height, color)
 				// Adjust the height and y position according to your needs
-				this.driver.fillRect(startX, this.height - 22, totalTextWidth, 10, 0); // 0 for black
+				this.driver.fillRect(startX, this.height - 30, totalTextWidth, 10, 0); // 0 for black
 			
-				this.driver.setCursor(startX, this.height - 22);
+				this.driver.setCursor(startX, this.height - 30);
 				this.driver.writeString(fonts.monospace, 1, this.data.trackType, 4);
 			}
 		  
